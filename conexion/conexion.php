bb211e69da294204f3572a4a93b5f33f4ef5d1a8
Julian-Yam <2005436@utrivieramaya.edu.mx>
2023-10-31 15:44:02 -0500

    Actualizacion del index

---
+++ conexion/conexion.php
@@ -0,0 +1,30 @@
+<?php 
+ 
+$servidor = "localhost"; // El servidor que utilizamos  
+$usuario = "valentin"; // El usuario  
+$contraseña = "valentin1"; // La contraseña del usuario 
+$BD = "valentin_chatbot"; // El nombre de la base de datos 
+ 
+/* 
+Aquí abrimos la conexión en el servidor. El @ que se ponde delante de la funcion, es para que no muestre el error al momento de ejecutarse, ya crearemos un código para eso
+*/
+ 
+$conexion = @mysqli_connect($servidor, $usuario, $contraseña); 
+ 
+/* 
+Aquí preguntamos si la conexión no pudo realizarse, de ser así lanza un mensaje en la pantalla con el siguiente texto "No pudo conectarse:" y le agrega el error ocurrido con "mysql_error()" 
+*/
+ 
+if (!$conexion) { 
+    die('<strong>No pudo conectarse:</strong> ' . mysqli_error($conexion)); 
+}else{ 
+    // Lo siguiente indica que la conexión ha sido realizada 
+    echo 'Conectado  satisfactoriamente al servidor <br />'; 
+} 
+/* 
+En esta linea seleccionaremos la BD con la que trabajaremos y le pasaremos como referencia la conexión al servidor. 
+*/
+ 
+mysqli_select_db( $BD, $conexion) or die(mysqli_error($conexion)); 
+ 
+?>
