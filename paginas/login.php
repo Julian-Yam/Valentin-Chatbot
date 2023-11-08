eb1a190087633e71b2a4d28ddff455db6524594a
Julian-Yam <2005436@utrivieramaya.edu.mx>
2023-11-08 16:43:46 -0500

    inicio de chatbot

---
+++ paginas/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div class="login">
+        <h1>Inicio de sesion</h1>
+        <form method="post">
+            <div class="username">
+                <input type="text" required>
+                <label>Nombre de usuario</label>
+            </div>
+            <div class="contrasena">
+                <input type="password" required>
+                <label>Contraseña</label>
+            </div>
+            <div class="recordar">¿Olvido su contraseña?</div>
+            <input type="submit" value="Iniciar">
+            <div class="registrarse">
+                <a href="#">Registrarse</a>
+            </div>
+        </form>
+    </div>
+    
+</body>
+</html>
